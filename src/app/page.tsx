cd0e15c01cc26c43226f1536d8784ce3bebd5bfd
Radeori <radeori@gmail.com>
2024-10-11 16:31:52 -0300

    Aprimorado hover do botao Search stations

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -225,12 +225,14 @@ export default function Home() {
                 onClick={() => setSidebarVisible(true)}
                 onMouseEnter={() => setSearchHover(true)} onMouseLeave={() => {setSearchHover(false);setSearchActive(false)}}
                 onMouseDown={() => setSearchActive(true)} onMouseUp={() => setSearchActive(false)}
-                className={"bi bi-search " + styles.listSearchIcon + (searchHover ? " " + styles.listSearchHover : "") + (searchActive ? " " + styles.listSearchActive : "")}>
+                className={"bi bi-search " + styles.listSearchIcon + (searchHover && !sidebarVisible ? " " + styles.listSearchHover : "") +
+                  (searchActive && !sidebarVisible ? " " + styles.listSearchActive : "")}>
               </i>
               <span onClick={() => setSidebarVisible(true)}
                 onMouseEnter={() => setSearchHover(true)} onMouseLeave={() => {setSearchHover(false);setSearchActive(false)}}
                 onMouseDown={() => setSearchActive(true)} onMouseUp={() => setSearchActive(false)}
-                className={"hidden-xs " + styles.listSearchText}>
+                className={"hidden-xs " + styles.listSearchText + (searchHover && !sidebarVisible ? " " + styles.listSearchTextHover : "") +
+                  (searchActive && !sidebarVisible ? " " + styles.listSearchTextActive : "")}>
                 Search stations
               </span>
             </div>
